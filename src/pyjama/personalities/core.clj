658d69e8c9dc9c88c96104cb55644cfb64b0c67d
Nicolas Modrzyk <hellonico@gmail.com>
2024-12-21 18:35:09 +0900

    - dad personality

--- src/pyjama/personalities/core.clj
+++ src/pyjama/personalities/core.clj
@@ -7,6 +7,8 @@
      (let [
            config (if (map? _config) (merge _config pconfig) pconfig)
            input (if (map? _config) (:input _config) _config)
+           template (or (:template config) "%s")
+           realtime (true? (:realtime config))
            ]
      (pyjama.core/ollama
        (or (:url config) (System/getenv "OLLAMA_URL") "http://localhost:11434")
@@ -14,20 +16,27 @@
        {
         :model  (or (:model config) (System/getenv "PYJAMA_MODEL")  "llama3.2")
         :system (:system config)
-        :stream (:realtime config)
+        ;:template template
+        :stream realtime
         :prompt
-        (format
-          (:template config)
-          input)
+        (if (vector? input)
+          (apply format template input)
+          (format template input))
         }
-       (if (:realtime config) pyjama.core/print-generate-tokens :response)))))
+       (if realtime pyjama.core/print-generate-tokens :response)))))
 
 (def japanese-translator
   (make-personality
-    {:system "Translate each sentence from Japanese to English with no explanation and no other text than the translation. "
-     :template "%s"}))
+    {:system "Translate each sentence from Japanese to English with no explanation and no other text than the translation. "}))
 
 (def samuraizer
   (make-personality
-    {:system "Summarize each text you are getting, with no explanation and no other text than the summary.  "
-     :template "%s"}))
+    {:system "Summarize each text you are getting, with no explanation and no other text than the summary."}))
+
+(def three-points
+  (make-personality
+    {:system "Summarize the whole text you are getting in list of three points maximum, YOU CAN NOT WRITE MORE THAN THREE SENTENCES IN YOUR WHOLE ANSWER, with no explanation and no other text than the summary."}))
+
+(def dad
+  (make-personality
+    {:system "You are a Dad explaning things to your teenager son/daughter. You cannot use difficult words, and you answer with great details and humour the question you are being asked."  }))
