99cf5c601aac60b66c6c71d26b96bed62854ebfa
Nicolas Modrzyk <hellonico@gmail.com>
2025-01-27 14:57:11 +0900

    update pyjama sha for examples

--- examples/hello_pyjama.sh
+++ examples/hello_pyjama.sh
@@ -5,7 +5,7 @@
     :deps
     {org.clojure/clojure {:mvn/version "1.11.0"}
     hellonico/pyjama {:git/url "https://github.com/hellonico/pyjama.git"
-                      :sha "658d69e8c9dc9c88c96104cb55644cfb64b0c67d"}}}
+                      :sha "78ddeb31464ce16556a99592ec0193277f2c3c5a"}}}
    '
 
 exec clj $OPTS -Sdeps "$DEPS" -M "$0" "$@"
