780b72154e702d4c215a28a53e0797a22d773703
Nicolas Modrzyk <hellonico@gmail.com>
2025-01-28 17:48:00 +0900

    parallel was not parallel and terminal css fixes

--- src/pyjama/parallel.clj
+++ src/pyjama/parallel.clj
@@ -1,5 +1,6 @@
 (ns pyjama.parallel
-  (:require [pyjama.core]))
+  (:require [clojure.core.async :as async]
+            [pyjama.core]))
 
 (defn process-task [app-state task-id task-params task-atom]
   (swap! task-atom merge task-params)
@@ -12,22 +13,39 @@
 
 (defn process-tasks [app-state tasks]
   (let [task-atoms (mapv (fn [task] (atom {:id (:id task) :params (:params task) :result nil})) tasks)
-        futures (mapv (fn [[task task-atom]]
-                        (future
-                          (let [result (process-task app-state (:id task) (:params task) task-atom)]
-                            (integrate-task app-state (:id task) task-atom))))
-                      (map vector tasks task-atoms))]
-    {:task-atoms task-atoms :futures futures}))
+        task-chan (async/chan)]                             ;; Channel to send tasks
+    ;; Enqueue tasks onto the channel
+    (doseq [[task task-atom] (map vector tasks task-atoms)]
+      (async/go
+        (integrate-task app-state (:id task) task-atom)     ;; Integrate task
+        (let [result (process-task app-state (:id task) (:params task) task-atom)]
+          (integrate-task app-state (:id task) task-atom)   ;; Integrate task
+          (async/>! task-chan {:id (:id task) :result result})))) ;; Send result to the channel
+    {:task-atoms task-atoms :task-chan task-chan}))
+
+
+(defn clean-state [app-state]
+  (swap! app-state assoc :tasks {}))
 
 (defn parallel-generate [app-state config callback-one callback-all]
+  (clean-state app-state)
   (let [{:keys [models pre prompts]} config
         tasks (map-indexed (fn [i [model prompt]]
                              {:id i :params (cond-> {:model model :prompt prompt}
                                                     pre (assoc :pre pre))})
                            (for [model models prompt prompts]
-                             [model prompt]))]
-    (let [{:keys [task-atoms futures]} (process-tasks app-state tasks)]
-      (doseq [f futures]
-        (Thread/sleep 1000)
-        (callback-one @f))))
-  (callback-all))
+                             [model prompt]))
+        ;; Process tasks and get the task channel
+        {:keys [task-atoms task-chan]} (process-tasks app-state tasks)
+        result-chan (async/chan)]                           ;; Channel to collect all results
+    ;; Collect and process task results
+    (async/go
+      (loop [results []]
+        (if (< (count results) (count tasks))
+          (let [{:keys [id result]} (async/<! task-chan)]
+            (callback-one {:id id :result result})          ;; Trigger callback-one
+            (recur (conj results {:id id :result result}))) ;; Continue collecting
+          (do
+            (callback-all results)                          ;; Trigger callback-all when all are done
+            (async/close! result-chan)))))                  ;; Close the channel when done
+    nil))                                                   ;; Return nil since everything is handled asynchronously
--- test/morning/parallel_test.clj
+++ test/morning/parallel_test.clj
@@ -6,13 +6,18 @@
 
 (def ollama-url (or (System/getenv "OLLAMA_URL") "http://localhost:11434"))
 (deftest tinyllama-and-blue
-  (let [app-state (atom {:url ollama-url :tasks {}})]
-    ;(println "Starting parallel tasks...")
-    (parallel-generate
-      app-state
-      {
-       :models  ["tinyllama"]
-       :pre     "Explain in three points the following sentence:\n %s"
-       :prompts ["Why is the sky blue"]}
-      identity
-      (fn [] (clojure.pprint/pprint @app-state)))))
+  (let [processing (atom true)]
+    (let [app-state (atom {:url ollama-url :tasks {}})]
+      (parallel-generate
+        app-state
+        {
+         :models  ["tinyllama"]
+         :pre     "Explain in three points the following sentence:\n %s"
+         :prompts ["Why is the sky blue"]}
+        identity
+        (fn [_]
+          (clojure.pprint/pprint @app-state)
+          (reset! processing false)
+          )))
+    (while @processing
+      (Thread/sleep 1000))))
