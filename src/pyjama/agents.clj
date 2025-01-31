9fa6f29ab4f7c1f9d8dc6b6817090ae6d305bc6b
Nicolas Modrzyk <hellonico@gmail.com>
2025-01-31 14:04:03 +0900

    agents testing code

--- src/pyjama/agents.clj
+++ src/pyjama/agents.clj
@@ -3,29 +3,90 @@
             [pyjama.core]
             [pyjama.functions :refer [ollama-fn]]))
 
+
+
+(defn simple-log
+  ([id category message]
+   (println id "\t " category "\t" (apply str (subs (str message) 0 (min 100 (count (str message)))))))
+  ([id message]
+   (println id "\t " (apply str (subs (str message) 0 (min 100 (count (str message))))))))
+
 (defn create-broker
   []
   (let [topics (atom {})]
-    {:topics      topics
-     :subscribe   (fn [topic subscriber-chan]
-                    (swap! topics update topic #(conj (or % #{}) subscriber-chan)))
-     :unsubscribe (fn [topic subscriber-chan]
-                    (swap! topics update topic #(disj (or % #{}) subscriber-chan)))
-     :publish     (fn [topic message]
-                    (doseq [subscriber (get @topics topic)]
-                      (async/put! subscriber message)))}))
+    (letfn [(subscribe [topic subscriber-chan]
+              (swap! topics update topic #(conj (or % #{}) subscriber-chan)))
+
+            (unsubscribe [topic subscriber-chan]
+              (swap! topics update topic #(disj (or % #{}) subscriber-chan)))
+
+            (publish [topic message]
+              (doseq [subscriber (get @topics topic)]
+                (async/put! subscriber message)))
+
+            (forward [from-topic to-topic]
+              (let [forward-chan (async/chan)]
+                (subscribe from-topic forward-chan)
+                (async/go-loop []
+                  (when-let [message (async/<! forward-chan)]
+                    (publish to-topic message))
+                  (recur))))]
+
+      {:topics      topics
+       :subscribe   subscribe
+       :unsubscribe unsubscribe
+       :publish     publish
+       :forward     forward})))
+
+
+
+
+(defn make-agent
+  [id broker config callback]
+  (let [input-chan (async/chan)
+        output-chan (async/chan)]
+    (async/go-loop []
+      (let [message (async/<! input-chan)]
+        (when message
+          (try
+            (simple-log id :in message)
+            (let [out (callback message broker)]
+              (if (not (nil? out))
+                (do
+                  ((:publish broker) output-chan out)
+                  (simple-log id :out out)))
+              )
+            (catch Exception e
+              (.printStackTrace e)
+              (println "Error processing Agent:" (.getMessage e)))))
+        (recur)))
+    {:id          id
+     :output-chan output-chan
+     :input-chan  input-chan}))
+(def make-simple-agent make-agent)
 
 (defn make-ollama-agent
   [id broker config callback]
   (let [input-chan (async/chan)
+        output-chan (async/chan)
         ollama-helper (ollama-fn config)]
     (async/go-loop []
-                   (let [message (async/<! input-chan)]
-                     (when message
-                       (try
-                         (callback message ollama-helper broker)
-                         (catch Exception e
-                           (println "Error processing Ollama response:" (.getMessage e)))))
-                     (recur)))
-    {:id         id
-     :input-chan input-chan}))
+      (let [message (async/<! input-chan)]
+        (simple-log id :in message)
+        (when message
+          (try
+            (let [out (callback message ollama-helper broker)]
+              ((:publish broker) output-chan out)
+              (simple-log id :out (apply str out)))
+            (catch Exception e
+              (println "Error processing Ollama response:" (.getMessage e)))))
+        (recur)))
+    {:id          id
+     :output-chan output-chan
+     :input-chan  input-chan}))
+
+(defn make-logger-agent [id broker config callback]
+  (make-simple-agent id broker config (fn [message _] (println message))))
+
+(defn make-echo-agent [id broker config callback]
+  (make-simple-agent id broker config (fn [message _] message)))
---
+++ test/morning/agents.clj
@@ -0,0 +1,22 @@
+(ns morning.agents
+  (:require [clojure.test :refer :all]
+            [pyjama.agents :as a]))
+
+(defn setup-system! [broker]
+  (let [agent-1 (a/make-echo-agent "echo" broker {} nil)
+        agent-2 (a/make-logger-agent "log" broker {} nil)]
+
+    ((:subscribe broker) :trip-request-in (:input-chan agent-1))
+    ((:forward broker) (:output-chan agent-1) :trip-request-out)
+    ((:subscribe broker) :trip-request-out (:input-chan agent-2))
+
+    ;((:publish broker) :trip-request-in "Test Message")
+
+    ))
+
+(deftest simple-agents-system
+  (let [broker (a/create-broker)]
+    (setup-system! broker)
+
+    ((:publish broker) :trip-request-in {:from "Paris" :to "Tokyo" :date "2025-02-10"})
+    (Thread/sleep 5000)))
