8504bc0e2c19e43b686dd88a363500ae85221112
Nicolas Modrzyk <hellonico@gmail.com>
2025-02-11 23:25:48 +0900

    embeddings for one pdf + test

--- src/pyjama/embeddings.clj
+++ src/pyjama/embeddings.clj
@@ -3,19 +3,27 @@
     [mikera.vectorz.core :as vectorz]
     [pyjama.core]))
 
-(defn generate-embeddings [url model sentences]
+(defn chunk-text [text chunk-size]
+  (map #(apply str %) (partition-all chunk-size text)))
+
+(defn generate-embeddings [{:keys [url embedding-model]} chunks]
   (pyjama.core/ollama
     url
     :embed
-    {:model model :input sentences}))
+    {:model embedding-model
+     :input chunks}))
 
-(defn generate-vectorz-documents [{:keys [url documents embedding-model]}]
-  (let [embeddings (generate-embeddings url embedding-model documents)]
+; TODO: chunk strategy to be implemented here
+(defn generate-vectorz-documents [{:keys [chunk-size documents] :as config}]
+  (let [chunks (if (vector? documents)
+                 documents
+                 (chunk-text documents (or chunk-size 2048)))
+        embeddings (generate-embeddings config chunks)]
     (map-indexed (fn [idx sentence]
                    {:id        (inc idx)
                     :content   sentence
                     :embedding (vectorz/vec (get embeddings idx))})
-                 documents)))
+                 chunks)))
 
 (defn cosine-similarity [v1 v2]
   (let [dot-product (vectorz/dot v1 v2)
@@ -30,7 +38,7 @@
        (sort-by :similarity >)
        (take top-n)))
 
-(defn enhance-prompt [{:keys [base-prompt question documents url top-n] :as input}]
+(defn enhanced-context [{:keys [question documents url top-n] :as input}]
   (let [input (assoc input :documents [question])
         query-embedding (:embedding (first (generate-vectorz-documents input)))
         top-docs (top-related-documents documents query-embedding top-n)
@@ -38,4 +46,4 @@
                   top-docs
                   (map :content)
                   (clojure.string/join "\n"))]
-    (str "Context:\n" context "\n\n" base-prompt "\n\n" question)))
+    context))
--- test/morning/embeddings_test.clj
+++ test/morning/embeddings_test.clj
@@ -1,37 +1,63 @@
 (ns morning.embeddings-test
-  (:require [clojure.core]
+  (:require [clojure.pprint]
             [clojure.test :refer :all]
             [pyjama.core]
-            [pyjama.embeddings :refer [enhance-prompt generate-vectorz-documents]]))
+            [pyjama.embeddings :refer [enhanced-context generate-vectorz-documents]]))
 
-(def url (or (System/getenv "OLLAMA_URL") "http://localhost:11434"))
-(def embedding-model "granite-embedding")
+(def url (or (System/getenv "OLLAMA_URL")
+             "http://localhost:11432"))
+(def embedding-model
+  "granite-embedding")
 
 (deftest pull-embeddings-model
   (->
     (pyjama.core/ollama url :pull {:model embedding-model})
     (println)))
 
-(deftest some-embeddings
-  (let [sentences ["The sky is blue because the smurfs are blue."
-                   "The sky is red in the evening because the grand smurf is too."]
-        documents (generate-vectorz-documents
-                    {:documents sentences :url url :embedding-model embedding-model})
-        question "why is the sky blue?"
-        base-prompt "Answer the coming question using no previous knowledge and ONLY knowledge from the context."
-        enhanced_prompt (enhance-prompt
-                          {
-                           :base-prompt     base-prompt
-                           :question        question
-                           :url             url
-                           :embedding-model embedding-model
-                           :top-n           1
-                           :documents       documents
-                           })
-        _ (println enhanced_prompt)
-        ]
-    (clojure.pprint/pprint
+(defn rag [config]
+  (let [
+        documents
+        (generate-vectorz-documents
+          (select-keys config [:documents :url :chunk-size :embedding-model]))
+        ;_ (pprint-to-file "documents.edn" documents)
+        ;documents (load-file "documents.edn")
+        ;_ (println documents)
+        enhanced-context
+        (enhanced-context
+          (assoc
+            (select-keys config
+                         [:question :url :embedding-model :top-n])
+            :documents documents
+            ))]
+    ;(clojure.pprint/pprint
       (pyjama.core/ollama
         url
         :generate
-        {:model "tinyllama" :prompt enhanced_prompt}))))
+        (assoc
+          (select-keys config [:options :stream :model :pre])
+          :prompt [enhanced-context (:question config)]
+          ))
+    ;)
+    ))
+
+(deftest some-embeddings
+  (let [text "The sky is blue because the smurfs are blue.
+              The sky is red in the evening because the grand smurf is too."
+        pre "Context: \n\n
+        %s.
+        \n\n
+        Answer the question:
+        %s
+        using no previous knowledge and ONLY knowledge from the context. No comments.
+        Make the answer as short as possible."
+        question "why is the sky red?"
+        ]
+    (rag {:pre             pre
+          :url             url
+          :model           "mistral"
+          :stream          true
+          :chunk-size      4096
+          :top-n           1
+          :question        question
+          :documents       text
+          :embedding-model embedding-model})))
