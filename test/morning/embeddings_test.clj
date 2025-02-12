cbee644ec7b380e0aedc73d616f98cace174726e
Nicolas Modrzyk <hellonico@gmail.com>
2025-02-12 16:26:56 +0900

    calvin and hobbes

--- test/morning/embeddings_test.clj
+++ test/morning/embeddings_test.clj
@@ -18,22 +18,24 @@
 (def source-of-truth
   (pyjama.utils/load-lines-of-file "test/morning/source_of_truth.txt"))
 
-(deftest some-embeddings
+; TODO: write post with
+; ;https://scontent-nrt1-2.xx.fbcdn.net/v/t39.30808-6/476351016_593111663543676_6030097482521434681_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=aa7b47&_nc_ohc=nTDiTn9oq3cQ7kNvgEGU6NG&_nc_oc=Adi_LpQvOfcGC-Xt94yVHwmYitM5kiqMTDgfFnqhtaE3PXrCjCyUhvx2HV9naO6Bokw&_nc_zt=23&_nc_ht=scontent-nrt1-2.xx&_nc_gid=Atl2N66PthIQyToLVq7l3JM&oh=00_AYDoLNDPjJGyUdRvLcDbrwwP5dkDFGYg9qIpBbm_K66tcA&oe=67B2237A
+(deftest some-embeddings-and-rag
   (let [embed-config {:url             url
-                      :chunk-size      300
+                      :chunk-size      500
                       :documents       source-of-truth
                       :embedding-model embedding-model}
         documents (pyjama.embeddings/generate-vectorz-documents embed-config)
 
-        question "why is the sky red?"
+        question "why ice floats?"
 
         pre "Context: \n\n
         %s.
         \n\n
         Answer the question:
         %s
-        using no previous knowledge and ONLY knowledge from the context. No comments.
-        Make the answer as short as possible."
+        using no previous knowledge and ONLY knowledge from the context.
+        "
 
         config (assoc embed-config
                  :question question
@@ -44,4 +46,8 @@
                  :pre pre)
 
         ]
-    (println (pyjama.embeddings/simple-rag config documents))))
+    (println
+      (pyjama.embeddings/simple-rag config documents))))
+
+;According to the context, ice floats because it's cold.
+;The reason it wants to get warm is so that it can be nearer to the sun...
